7a613461835d5784f58879ffa132b46073dceee2
Richa <richa.lal09@gmail.com>
2020-04-05 18:11:40 +0530

    First Git Commit in Local for HelloWorld

---
+++ Week2/src/main/Helloworld.java
@@ -0,0 +1,28 @@
+public class Helloworld
+{
+    public String s1;
+    public Helloworld(String s1) {
+        this.s1 = s1;
+    }
+
+    public String getS1() {
+        return s1;
+    }
+
+    public void Happy(String s1)
+    {
+        if ( s1.equals("HAPPY"))
+        {
+            System.out.println("I am Happy Today");
+        }
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("I am luck today");
+    }
+
+}
+
+
+
+
